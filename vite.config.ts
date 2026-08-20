3bf337565d761c6531118db3d6df07c2ab88a324
DUUUINGDUCK <olanbad5@gmail.com>
2026-08-20 17:38:17 -0400

    Add Vite extension build configuration

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        background: "src/background.ts",
+        content: "src/content.ts",
+        devtools: "ui/devtools.html",
+        popup: "ui/popup.html"
+      },
+      output: { entryFileNames: "dist/[name].js", chunkFileNames: "dist/chunks/[name].js", assetFileNames: "dist/assets/[name][extname]" }
+    }
+  }
+});
