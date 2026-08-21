0ade2e0ef486ed88327dabb56fa623f49bf52e05
DUUUINGDUCK <olanbad5@gmail.com>
2026-08-20 23:04:59 -0400

    Fix Vite extension build entries

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+
 export default defineConfig({
   build: {
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         background: "src/background.ts",
@@ -8,7 +10,11 @@ export default defineConfig({
         devtools: "ui/devtools.html",
         popup: "ui/popup.html"
       },
-      output: { entryFileNames: "dist/[name].js", chunkFileNames: "dist/chunks/[name].js", assetFileNames: "dist/assets/[name][extname]" }
+      output: {
+        entryFileNames: "dist/[name].js",
+        chunkFileNames: "dist/chunks/[name].js",
+        assetFileNames: "dist/assets/[name][extname]"
+      }
     }
   }
 });
